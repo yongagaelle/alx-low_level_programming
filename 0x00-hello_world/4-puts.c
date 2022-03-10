9b9e69175189ccad10884a7a2899c4ccbcfff7fe
yongagaelle <yongagaellemanuela@gmail.com>
2022-03-10 01:36:29 -0800

    4-hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /** 
- * main - puts Programming is like building a multilingual puzzle
+ * main - prints Programming is like building a multilingual puzzle
  *
  * Return:  always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+      puts("\"Programming is like building a multilingual puzzle");
+
+      return (0);
 }
