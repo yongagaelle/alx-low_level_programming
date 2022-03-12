7179b78ba9e22fad580979506764d09137ead38b
yongagaelle <yongagaellemanuela@gmail.com>
2022-03-12 06:04:48 -0800

    the alphabet in lowercase and in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and in uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+	return (0);
+}
+ 
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 0-positive anything is better than negative nothing
 1- The last digit
 2- I sometimes suffer from insomnia.
+3- alphABET
