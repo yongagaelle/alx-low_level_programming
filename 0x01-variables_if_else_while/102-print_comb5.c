90a6e04e8bb1543011009e29f8fe4910ea26ef06
yongagaelle <yongagaellemanuela@gmail.com>
2022-03-12 14:57:02 -0800

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int n1, n2;
 
-	for (n1 = 0; n1 < 98; n1++)
+	for (n1 = 0; n1 <= 98; n1++)
 	{
-		for (n2 = n1 + 1; n2 < 99; n2++)
+		for (n2 = n1 + 1; n2 <= 99; n2++)
 		{
 			putchar((n1 / 10) + '0');
 			putchar((n1 % 10) + '0');
