d3ce8374e43b0483eb1eff74390dd16b9b4ba122
yongagaelle <yongagaellemanuela@gmail.com>
2022-03-12 14:20:35 -0800

    prints possible different combinaisons

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int n1, n2;
 
-	for (n1 = 0; n1 < 10; n1++)
+	for (n1 = 0; n1 < 9; n1++)
 	{
-		for (n2 = 0; n2 < 10; n2++)
+		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
 			putchar((n2 % 10) + '0');
 
-			if (n1 == 9 && n2 == 9)
+			if (n1 == 8 && n2 == 9)
 				continue;
 
 			putchar(',');
