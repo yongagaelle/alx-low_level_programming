76dbda6158f1ba6a39c7a822857b5a5f93a05e9e
unknown <yongagaellemanuela@gmail.com>
2022-03-16 00:21:56 +0200

    mains multiple

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- *
- * Desc: Header file containing declarations for all functions
- *       used in the 0x03-debugging.
- */
 #include <stdio.h>
 
 void positive_or_negative(int i);
