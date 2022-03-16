448d730b5337593519d1aa92cee9bfd9761be15c
unknown <yongagaellemanuela@gmail.com>
2022-03-16 23:53:08 +0200

    mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,14 @@
 /**
  * main - check code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "main.h"
 
 /**
  * positive_or_negative - prints number
- * @i: The number to print
+ * @i: the number to print
  */
-
 void positive_or_negative(int i)
 {
 
