008832ac18b450e7b1450e540827fc64ff7e4d44
yongagaelle <yongagaellemanuela@gmail.com>
2022-03-21 10:30:12 -0700

    generate key

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,11 +9,44 @@
  */
 int main(void)
 {
-	int i;
+	int index = 0, sum = 0, diff1, diff2;
+	char password[90];
 
-	srand(time(NULL));
-	for (i = 0; i <= 37; i++)
-		putchar(rand() % (122 - 33) + 33);
+	srand(time(0));
 
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff1 = (sum - 2772) / 2;
+		diff2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff1))
+			{
+				password[index] -= diff1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff2))
+			{
+				password[index] -= diff2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
 	return (0);
 }
