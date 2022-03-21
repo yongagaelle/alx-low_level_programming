07b03b4d628abfd992cfd1404f72a8092f89a3bd
unknown <yongagaellemanuela@gmail.com>
2022-03-21 14:39:26 +0200

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed.
+ */
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1- don't swap horses in crossing a stream
 2- this report, by its very length, defends itself against the risk of being read
 3- i do not fear computer. I fear the lack of them
+4- i can only go one way. i've not got a reverse gear
