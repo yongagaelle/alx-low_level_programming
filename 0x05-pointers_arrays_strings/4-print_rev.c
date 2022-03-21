7985df2b13f8023614c4cd19233ef4232f55b0fc
unknown <yongagaellemanuela@gmail.com>
2022-03-21 15:06:52 +0200

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		len++;
+	while (s[i] != '\0')
+		i++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+        for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
 	_putchar('\n');
 }
