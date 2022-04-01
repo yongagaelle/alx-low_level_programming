705d9e5b1164a593c0bf32e7c92ea69687f88568
yongagaelle <yongagaellemanuela@gmail.com>
2022-04-01 09:56:07 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - enter point.
+  *
+  * @argc: number of arguments passed in call to program
+  * @argv: array of arguments
+  *
+  * Return: 0 always
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-it ain't what they call you,it's what you answer to
