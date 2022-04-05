2c7d7e742f80bff94c27f9e83187296cad6159b3
yongagaelle <yongagaellemanuela@gmail.com>
2022-04-04 17:19:41 -0700

    allocation grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: first dimension of the array
+ * @height: second dimension of the array
+ *
+ * Return: a pointer to 2-dimensional array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **tab;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	tab = malloc(sizeof(int *) * height);
+	if (tab == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		tab[i] = malloc(sizeof(int) * width);
+		if (tab[i] == NULL)
+		{
+			while (i--)
+			       free(tab[i]);
+
+		        free(tab);
+		        return (NULL);
+		}
+	        
+	        for (j = 0; j < width; j++)
+	                    tab[i][j] = 0;
+	}
+
+        return (tab);
+}	
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0-float like a butterfly,sting like a bee
 1-The woman who has no imagination has a wings
 2-he who is not courageous enough to take risks
+3-if you even dream of beating me
