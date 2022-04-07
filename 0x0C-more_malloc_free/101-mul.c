606797bd1d08b221f1a6fabb3d3ae0bc6ba1a081
yongagaelle <yongagaellemanuela@gmail.com>
2022-04-07 16:52:35 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,104 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * exit_x_error - print error message and exit of program
- *
- * Return: nothing.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void exit_x_error(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * number_str_len - returns the length of string numbers or exit
- *
- * @string: pointer to string.
- * Return: length of string.
- */
-int number_str_len(char *string)
-{
-	int len = 0;
-
-	while (string[len])
-	{
-		if (string[len] < '0' || '9' < string[len])
-			exit_x_error();
-		len++;
-	}
-	return (len);
-}
-
-/**
- * print_number_string - print number of string
- *
- * @array_respuesta: pointer to string.
- * @len: Length of the string
- *
- * Return: nothing.
- */
-void print_number_string(char *array_respuesta, int len)
-{
-	char printed = 0;
-	int i;
-
-	for (i = 0; i < len; i++)
-		if (printed || ('0' < array_respuesta[i] && array_respuesta[i] <= '9'))
-		{
-			_putchar(array_respuesta[i]);
-			printed = 1;
-		}
-
-	if (!printed)
-		_putchar('0');
-	_putchar('\n');
-}
-
-/**
- * main - enter point
- *
- * @argc: number of parameters passed to program
- * @argv: array of parameters
- *
- * Return: 0 on succes
+ * main - multiplies two positive numbers and print result
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, success
  */
 int main(int argc, char *argv[])
 {
-	int len1 = 0, len2 = 0, sum = 0, index1, index2, i;
-	char *num1, *num2, *array_respuesta;
+	unsigned long mul;
+	int i, j;
 
 	if (argc != 3)
-		exit_x_error();
-
-	num1 = argv[1];
-	num2 = argv[2];
-	len1 = number_str_len(num1);
-	len2 = number_str_len(num2);
-
-	array_respuesta = malloc(sizeof(char) * (len1 + len2));
-	if (array_respuesta == NULL)
-		exit_x_error();
-
-	index1 = len1 - 1;
-	for (i = len1 + len2 - 1; i > 0; i--, index1--)
 	{
-		for (index2 = 0; index2 < len2; index2++)
-		{
-			if (index2 < (len1 - index1) && (index1 + index2) >= 0)
-				sum += (num1[index1 + index2] - '0') * (num2[len2 - index2 - 1] - '0');
-		}
-		array_respuesta[i] = (sum % 10) + '0';
-		sum /= 10;
+		printf("Error\n");
+		exit(98);
 	}
-	if (sum != 0)
-		array_respuesta[i] = sum + '0';
-
-	print_number_string(array_respuesta, (len1 + len2));
-
-	free(array_respuesta);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if ((argv[i][j]) < '0' && (argv[i][j]) > '9')
+			{
+				printf("Error\n"), exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
