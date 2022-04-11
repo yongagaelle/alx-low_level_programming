a5680460f6c1f6c33b827c5f6b8e7c86e1953e41
yongagaelle <yongagaellemanuela@gmail.com>
2022-04-11 15:25:53 -0700

    dog

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - countain info of a dog
+ * @name: pointer to name of dog
+ * @age: age
+ * @owner: pointer to string owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
