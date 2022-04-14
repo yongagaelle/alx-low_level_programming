72f6a1ddb295583d0c3440bfa5ccbc4e5b50c1bd
yongagaelle <yongagaellemanuela@gmail.com>
2022-04-13 23:03:16 -0700

    my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+
 #endif
